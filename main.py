727fea4634e9c41c287b6fa6479f8e4f464cf9ce
Iwo Mateusz Dubaniowski <iwomateuszdubaniowski@Iwos-MacBook-Pro.local>
2016-03-09 18:31:46 +0800

    Developed simulation initialization mechanics

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ consumed=0
 
 #Assigns the energy consumption and production values + production, - consumption.
 #This is done in dimensionless units for the purpose of this simulation
-counter=1
+counter=0
 for i in g.vs:
     if counter%10==0:
         i["cap"]=20*random.randint(100, 500)
@@ -47,10 +47,56 @@ for i in g.vs:
     counter=counter+1
 sum=prod+consumed
 
+#Calculating average minimum bandwidth(capacity) of an edge
+avgMinEdgeBandwidth=prod/numEdg
+
+for i in g.es:
+    i["cap"]=avgMinEdgeBandwidth
+
+#Number of edges attached to each vertice
+numEdgesAtt=[]
+for i in g.vs:
+    sumCon=0
+    edges_attached=g.incident(i, mode="ALL")
+    numEdgesAtt+=[len(edges_attached)]
+
+#Ensuring the capacity of connections is sufficient to carry load
+for i in g.vs:
+    sumCon=0
+    edges_attached=g.incident(i, mode="ALL")
+    for j in edges_attached:
+        sumCon=sumCon+g.es[j]["cap"]
+    if sumCon<abs(i["cap"]):
+        for j in edges_attached:
+            g.es[j]["cap"]=g.es[j]["cap"]+((abs(i["cap"])-sumCon+10)/len(edges_attached))
+
+#Printing intermediate result
+for i in range(0, 10):
+    print("Capacity of edge %d: %.2f" %(i, g.es[i]["cap"]))
+
+#Total sum of capacities connected to each vertice
+finalSumsCon=[]
+for i in g.vs:
+    sumCon=0
+    edges_attached=g.incident(i, mode="ALL")
+    for j in edges_attached:
+        sumCon=sumCon+g.es[j]["cap"]
+    finalSumsCon+=[sumCon]
+
+#Calculating extra capacity available to each vertice
+extraCapacity=np.subtract(finalSumsCon, g.vs[:]["cap"])
+myFormattedList = [ '%.2f' % elem for elem in extraCapacity ]   #Formating doubles
+print(myFormattedList[0:20], numEdgesAtt[0])                    #Printing intermediate stat
+
+avgEdgeBandwidth=np.sum(g.es[:]["cap"])/numEdg
+        
 #Network energy data aggregates presented
 print("Net energy value: ", sum)
 print("\tEnergy produced: ", prod)
 print("\tEnergy consumed: ", consumed)
+print("Average min edge bandwidth: %.2f" % avgMinEdgeBandwidth)
+print("Average edge bandwidth: %.2f" % avgEdgeBandwidth)
+
 
 #Displaying network
 #print ("Displaying network...")
