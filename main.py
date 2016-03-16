3dd1ec69832de7b63fbe15f3bd34592979d5a87f
Iwo Mateusz Dubaniowski <iwomateuszdubaniowski@vpn-global-dhcp3-138.ethz.ch>
2016-03-16 13:23:24 +0800

    Balancing of network pre and post removal of node completed

--- main.py
+++ main.py
@@ -9,9 +9,134 @@ import igraph
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import sys
 
 def displayNetwork(g, visual_style={}):
     igraph.plot(g, **visual_style)
+
+#Displaying network
+def displayGraph(g):
+    visual_ggg={}
+    visual_ggg["vertex_size"] = np.add(np.divide(np.absolute(g.vs[:]["capCur"]), 600), 7)
+    temp = np.divide(g.vs[:]["capCur"], np.absolute(g.vs[:]["capCur"]))
+    temps=[]
+    for i in range(0, len(temp)):
+        if temp[i] > 0:
+            temps+=["green"]
+        else:
+            temps+=["red"]
+    visual_ggg["vertex_color"] = temps
+    visual_ggg["edge_curved"] = 0
+    visual_ggg["edge_width"] = 0.75
+    #print ("Displaying network...")
+    displayNetwork(g, visual_ggg)
+
+def balanceNetwork(g, MAXCHNG):
+    err=0
+    allPos=0
+    allNeg=0
+    totAvCap=prod
+    maxChng=prod
+    xover=1
+    while allPos==0 and allNeg==0 and maxChng>MAXCHNG and xover==1:
+        if err>0:
+            break
+        allPos=1
+        allNeg=1
+        maxChng=0
+        xover=0
+        for i in g.es:
+            v1=i.source
+            v2=i.target
+            caps=[g.vs[v1]["capCur"], g.vs[v2]["capCur"]]
+            avg=(g.vs[v1]["capCur"]+g.vs[v2]["capCur"])/2
+            chng=-np.subtract(caps, [avg, avg])
+            capI=i["cap"]
+            inc1=g.vs[v1]["attCapUsed"][i.index]+chng[0]
+            inc2=g.vs[v2]["attCapUsed"][i.index]+chng[1]
+            inc1=round(inc1, 3)
+            inc2=round(inc2, 3)
+            avg=round(avg, 3)
+            chng[0]=round(chng[0], 3)
+            chng[1]=round(chng[1], 3)
+            if abs(inc1)-abs(inc2)>0.01:
+        	    print("Error! Numerical. Rounding exceeded.", abs(inc1)-abs(inc2))
+        	    err=1
+        	    break
+            inc=abs(inc1)
+            if capI>=inc:
+        	    g.vs[v1]["capCur"]+=chng[0]
+        	    g.vs[v2]["capCur"]+=chng[1]
+        	    g.vs[v1]["attCapUsed"][i.index]=inc1
+        	    g.vs[v2]["attCapUsed"][i.index]=-inc1
+            else:
+        	    if inc1>0:
+        		    inc1=capI
+        		    inc2=-capI
+        	    else:
+        		    inc1=-capI
+        		    inc2=capI
+        	    chng[0]=inc1-g.vs[v1]["attCapUsed"][i.index]
+        	    chng[1]=inc2-g.vs[v2]["attCapUsed"][i.index]
+        	    inc1=round(inc1, 3)
+        	    g.vs[v1]["attCapUsed"][i.index]=inc1
+        	    g.vs[v2]["attCapUsed"][i.index]=-inc1
+        	    g.vs[v1]["capCur"]+=chng[0]
+        	    g.vs[v2]["capCur"]-=chng[0]
+        	    g.vs[v1]["capCur"]=round(g.vs[v1]["capCur"], 3)
+        	    g.vs[v2]["capCur"]=round(g.vs[v2]["capCur"], 3)
+            if g.vs[v1]["capCur"]<0 or g.vs[v2]["capCur"]<0:
+        	    allPos=0
+            if g.vs[v1]["capCur"]>=0 or g.vs[v2]["capCur"]>=0:
+        	    allNeg=0
+            if abs(chng[1])>maxChng:
+                maxChng=abs(chng[1])
+            if abs(chng[0])>maxChng:
+                maxChng=abs(chng[0])
+            if (caps[0]<=0 and g.vs[v1]["capCur"]>=0) or (caps[1]<=0 and g.vs[v2]["capCur"]>=0):
+                xover=1
+            if (caps[0]>=0 and g.vs[v1]["capCur"]<=0) or (caps[1]>=0 and g.vs[v2]["capCur"]<=0):
+                xover=1
+            i["capCur"]=abs(inc1)
+        for h in g.vs:
+            tempsum=0
+            for hh in h["att"]:
+                tempsum=tempsum+g.es[hh]["cap"]-max(0, h["attCapUsed"][hh])
+            if (h["capCur"]+tempsum)<0:
+                print("Error! System unbalancable.")
+                err=2
+                break
+            tempsum=0
+            for hh in h["att"]:
+                tempsum=tempsum+g.es[hh]["cap"]+min(0, h["attCapUsed"][hh])
+            if (h["capCur"]-tempsum)>0:
+                totAvCap-=(h["capCur"]-tempsum)
+            if totAvCap<-consumed:
+                print("Error! System unbalancable.")
+                err=2
+                break
+        print(maxChng)
+            
+    print(totAvCap-consumed, v1, v2, avg, caps, chng, capI, i.index, inc1, inc2, inc)
+    print(allPos, allNeg, maxChng)
+    
+    #Printing info on the network state
+    allPos=1
+    balanced=False
+    for i in g.vs:
+        if i["capCur"]<0:
+            #print("ID: ", i["id"], " Cap: ", i["capCur"])
+            allPos=0
+    if allPos==1:
+        print("All vertices are satisfied!")
+        balanced=True
+    if allNeg==1:
+        print("Not enough load")
+    if allNeg==0 and allPos==0:
+        print("System unbalancable")
+    if xover==0 and not balanced:
+        print("No crossover!")
+    return [g, balanced]
     
 print ("Start simulation...")
 
@@ -74,9 +199,9 @@ for i in g.vs:
         for j in edges_attached:
             g.es[j]["cap"]=g.es[j]["cap"]+((abs(i["cap"])-sumCon+10)/len(edges_attached))
 
-
+#Adjust the connection capacity
 for i in g.es:
-    i["cap"]*=2
+    i["cap"]*=20
 
 #Printing intermediate result
 for i in range(0, 10):
@@ -97,123 +222,37 @@ for i in g.vs:
     finalSumsCon+=[sumCon]
 
 #Balance the network
-err=0
-allPos=0
-allNeg=0
-totAvCap=prod
-maxChng=prod
-xover=1
-while allPos==0 and allNeg==0 and maxChng>MAXCHNG and xover==1:
-    if err>0:
-        break
-    allPos=1
-    allNeg=1
-    maxChng=0
-    xover=0
-    for i in g.es:
-        v1=i.source
-        v2=i.target
-        caps=[g.vs[v1]["capCur"], g.vs[v2]["capCur"]]
-        avg=(g.vs[v1]["capCur"]+g.vs[v2]["capCur"])/2
-        chng=-np.subtract(caps, [avg, avg])
-        capI=i["cap"]
-        inc1=g.vs[v1]["attCapUsed"][i.index]+chng[0]
-        inc2=g.vs[v2]["attCapUsed"][i.index]+chng[1]
-        inc1=round(inc1, 3)
-        inc2=round(inc2, 3)
-        avg=round(avg, 3)
-        chng[0]=round(chng[0], 3)
-        chng[1]=round(chng[1], 3)
-        if abs(inc1)-abs(inc2)>0.01:
-    	    print("Error! Numerical. Rounding exceeded.", abs(inc1)-abs(inc2))
-    	    err=1
-    	    break
-        inc=abs(inc1)
-        if capI>=inc:
-    	    g.vs[v1]["capCur"]+=chng[0]
-    	    g.vs[v2]["capCur"]+=chng[1]
-    	    g.vs[v1]["attCapUsed"][i.index]=inc1
-    	    g.vs[v2]["attCapUsed"][i.index]=-inc1
-        else:
-    	    if inc1>0:
-    		    inc1=capI
-    		    inc2=-capI
-    	    else:
-    		    inc1=-capI
-    		    inc2=capI
-    	    chng[0]=inc1-g.vs[v1]["attCapUsed"][i.index]
-    	    chng[1]=inc2-g.vs[v2]["attCapUsed"][i.index]
-    	    inc1=round(inc1, 3)
-    	    g.vs[v1]["attCapUsed"][i.index]=inc1
-    	    g.vs[v2]["attCapUsed"][i.index]=-inc1
-    	    g.vs[v1]["capCur"]+=chng[0]
-    	    g.vs[v2]["capCur"]-=chng[0]
-    	    g.vs[v1]["capCur"]=round(g.vs[v1]["capCur"], 3)
-    	    g.vs[v2]["capCur"]=round(g.vs[v2]["capCur"], 3)
-        if g.vs[v1]["capCur"]<0 or g.vs[v2]["capCur"]<0:
-    	    allPos=0
-        if g.vs[v1]["capCur"]>=0 or g.vs[v2]["capCur"]>=0:
-    	    allNeg=0
-        if abs(chng[1])>maxChng:
-            maxChng=abs(chng[1])
-        if abs(chng[0])>maxChng:
-            maxChng=abs(chng[0])
-        if (caps[0]<=0 and g.vs[v1]["capCur"]>=0) or (caps[1]<=0 and g.vs[v2]["capCur"]>=0):
-            xover=1
-        if (caps[0]>=0 and g.vs[v1]["capCur"]<=0) or (caps[1]>=0 and g.vs[v2]["capCur"]<=0):
-            xover=1
-    for h in g.vs:
-        tempsum=0
-        for hh in h["att"]:
-            tempsum=tempsum+g.es[hh]["cap"]-max(0, h["attCapUsed"][hh])
-        if (h["capCur"]+tempsum)<0:
-            print("Error! System unbalancable.")
-            err=2
-            break
-        tempsum=0
-        for hh in h["att"]:
-            tempsum=tempsum+g.es[hh]["cap"]+min(0, h["attCapUsed"][hh])
-        if (h["capCur"]-tempsum)>0:
-            totAvCap-=(h["capCur"]-tempsum)
-        if totAvCap<-consumed:
-            print("Error! System unbalancable.")
-            err=2
-            break
-    print(maxChng)
-        
-print(totAvCap-consumed, v1, v2, avg, caps, chng, capI, i.index, inc1, inc2, inc)
-print(allPos, allNeg, maxChng)
-
-#Printing info on the network state
-allPos=1
-for i in g.vs:
-    if i["capCur"]<0:
-        #print("ID: ", i["id"], " Cap: ", i["capCur"])
-        allPos=0
-if allPos==1:
-    print("All vertices are satisfied!")
-if allNeg==1:
-    print("Not enough load")
-if allNeg==0 and allPos==0:
-    print("System unbalancable")
-if xover==0:
-    print("No crossover!")
+[g, balanced]=balanceNetwork(g, MAXCHNG)
 
 #Calculating extra capacity available to each vertice
 print("Extra(beyond demand) capacity available to first 21 vertices:")
-extraCapacity=np.subtract(finalSumsCon, np.absolute(g.vs[:]["cap"]))
+extraCapacity=np.subtract(finalSumsCon, np.absolute(g.vs[:]["capCur"]))
 myFormattedList = [ '%.2f' % elem for elem in extraCapacity ]   #Formating doubles
 print(myFormattedList[0:20], numEdgesAtt[0])                    #Printing intermediate stat
 
 avgEdgeBandwidth=np.sum(g.es[:]["cap"])/numEdg
-        
+extraTotalBandwidth=np.sum(g.es[:]["cap"])-np.sum(g.es[:]["capCur"])
+
+
 #Network energy data aggregates presented
 print("Net energy value: ", sum)
 print("\tEnergy produced: ", prod)
 print("\tEnergy consumed: ", consumed)
 print("Average initial min edge bandwidth: %.2f" % avgMinEdgeBandwidth)
 print("Average edge bandwidth: %.2f" % avgEdgeBandwidth)
+print("Extra bandwidth available in the whole network: ", extraTotalBandwidth)
 
+#Edges at peak bandwidth
+peakBandwidth=[]
+for i in g.es:
+    if i["capCur"]>=0.9*i["cap"]:
+        peakBandwidth+=[i.index]
+print("Edges close to or at peakBandwidth: ", peakBandwidth)
+
+displayGraph(g)
+
+if not balanced:
+    sys.exit()
 #Up to here we set up a network with given capacities.
 #Now, we will attempt to break it and see what happens.
 #Whether we can maintain supply of electricity to each vertice after nodes are removed.
@@ -224,14 +263,29 @@ print("We will remove node: ", nodeToRemove)
 
 g.delete_vertices(nodeToRemove)
 
+#Rebuild network data. Total capacities connected to each vertice.
 finalSumsCon=[]
+prod=0
+consumed=0
 for i in g.vs:
+    i["capCur"]=i["cap"]
     sumCon=0
     edges_attached=g.incident(i, mode="ALL")
+    i["att"]=edges_attached
+    temp=g.es[edges_attached]["cap"]
+    prod+=max(0, i["cap"])
+    consumed+=min(0, i["cap"])
+    for j in range(0, len(temp)):
+        temp[j]=0
+    i["attCapUsed"]=dict(zip(i["att"], temp))
     for j in edges_attached:
         sumCon=sumCon+g.es[j]["cap"]
     finalSumsCon+=[sumCon]
+for i in g.es:
+    i["capCur"]=0
 
+#Checking if network even has a chance of delivering electricity to each node
+networkCanHandleLoad=1
 counter=0
 adjuster=0
 extraCapacity=np.subtract(finalSumsCon, np.subtract(0, g.vs[:]["cap"]))
@@ -240,26 +294,37 @@ for i in extraCapacity:
         adjuster=1
     if i < 0:
         print("Not enough capacity in vertice: ", counter+adjuster, " missing: ", abs(i), " delivered: ", finalSumsCon[counter], " consumes: ", -g.vs[counter]["cap"])
+        networkCanHandleLoad=0
     counter+=1
 
+if networkCanHandleLoad==0:
+    print("Electricity supply network cannot handle removing node ", nodeToRemove, ". Capacity connected to this node is too small to satisfy the demand after node removed.")
+else:
+    print("We will now attempt balancing the network after node ", nodeToRemove, " is removed...")
+    [g, balanced]=balanceNetwork(g, MAXCHNG)
+    avgEdgeBandwidth=np.sum(g.es[:]["cap"])/numEdg
+    extraTotalBandwidth=np.sum(g.es[:]["cap"])-np.sum(g.es[:]["capCur"])
+
+
+    #Network energy data aggregates presented
+    print("Num edges: ", len(g.es), "Num vertices: ", len(g.vs))
+    print("Net energy value: ", sum)
+    print("\tEnergy produced: ", prod)
+    print("\tEnergy consumed: ", consumed)
+    print("Average edge bandwidth: %.2f" % avgEdgeBandwidth)
+    print("Extra bandwidth available in the whole network: ", extraTotalBandwidth)
+
+    #Edges at peak bandwidth
+    peakBandwidth=[]
+    for i in g.es:
+        if i["capCur"]>=0.9*i["cap"]:
+            peakBandwidth+=[i.index]
+    print("Edges close to or at peakBandwidth: ", peakBandwidth)
+    if balanced:
+        print("\nSUCCESS!\nRemoval of node ", nodeToRemove, "can be handled by this network.")
 
 print("Check", g.vs[0])
+print("Check2", g.es[0])
 #Done initial checking.
-
-#Displaying network
-visual_ggg={}
-visual_ggg["vertex_size"] = np.add(np.divide(np.absolute(g.vs[:]["capCur"]), 600), 7)
-temp = np.divide(g.vs[:]["capCur"], np.absolute(g.vs[:]["capCur"]))
-temps=[]
-for i in range(0, len(temp)):
-    if temp[i] > 0:
-        temps+=["green"]
-    else:
-        temps+=["red"]
-visual_ggg["vertex_color"] = temps
-visual_ggg["edge_curved"] = 0
-visual_ggg["edge_width"] = 0.75
-#print ("Displaying network...")
-displayNetwork(g, visual_ggg)
 
 print ("Simulation finished!")
